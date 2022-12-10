25e69a3e2e758ba1eb0f1bb66e0e28fbf37beb83
윤혜경 <sunrespect@naver.com>
2022-12-10 20:41:31 +0900

    update

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -92,26 +92,6 @@ var main = {
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
-    },
-    csave : function () {
-        var id = $('#id').val();
-
-        var data = {
-            comment: $('#comment').val()
-        };
-
-        $.ajax({
-            type: 'POST',
-            url: '/api/v1/posts/' + id + '/comments',
-            dataType: 'json',
-            contentType:'application/json; charset=utf-8',
-            data: JSON.stringify(data)
-        }).done(function() {
-            alert('댓글이 등록되었습니다.');
-            window.location.href = '/posts/detail/'+ id;
-        }).fail(function (error) {
-            alert(JSON.stringify(error));
-        });
     }
 };
 
